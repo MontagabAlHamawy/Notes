6cf4e732b7e24bb27b88c12ebc9fbbd10229c9ef
Montagab Al-Hamawy <ma0152260@gmail.com>
2023-09-21 16:04:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,105 +1,102 @@
-let input = document.querySelector( ".in" );
-let submit = document.querySelector( ".p" );
-let tasksdiv = document.querySelector( ".co" );
+let input = document.querySelector(".in");
+        let submit = document.querySelector(".p");
+        let tasksdiv = document.querySelector(".co");
 
+        let arrayofatasks = [];
 
-let arrayofatasks = [];
-
-if ( localStorage.getItem( "tasks" ) )
-{
-    arrayofatasks = JSON.parse( localStorage.getItem( "tasks" ) );
-}
-
-getdate();
-
-submit.onclick = function ()
-{
-    if ( input.value !== '' )
-    {
-        addTaskToArrey( input.value );
-        input.value = "";
-    }
-};
-
-tasksdiv.addEventListener( "click", ( e ) =>
-{
-    if ( e.target.classList.contains( "p3" ) )
-    {
-        delettaskw( e.target.parentElement.getAttribute( "data-id" ) );
-        e.target.parentElement.remove();
-    }
-    if ( e.target.classList.contains( "conte" ) )
-    {
-        torrlw( e.target.getAttribute( "data-id" ) )
-        e.target.classList.toggle( "done" );
-    }
-} )
-
-function addTaskToArrey ( tasktext )
-{
-    const task = {
-        id: Date.now(),
-        title: tasktext,
-        completed: false,
-    };
-    arrayofatasks.push( task );
-
-    addElementstopagefrom( arrayofatasks );
-
-    addDeta( arrayofatasks );
-};
-function addElementstopagefrom ( arrayofatasks )
-{
-
-    arrayofatasks.forEach( ( task ) =>
-    {
-        tasksdiv.innerHTML = ""
-        let div = document.createElement( "div" );
-        div.className = "conte";
-        if ( task.completed )
-        {
-            div.className = "conte done";
+        if (localStorage.getItem("tasks")) {
+            arrayofatasks = JSON.parse(localStorage.getItem("tasks"));
+            updateNoItemsText(); // تحديث نص "there are no items"
         }
-        div.setAttribute( "data-id", task.id );
-        let pp = document.createElement( "p" );
-        pp.appendChild( document.createTextNode( task.title ) );
-        pp.className = "p2";
-        div.appendChild( pp );
-        let span = document.createElement( "p" );
-        span.className = "p3";
-        span.appendChild( document.createTextNode( "delete" ) );
-        div.appendChild( span );
-        tasksdiv.appendChild( div );
-    } )
-}
-function addDeta ()
-{
-    window.localStorage.setItem( "tasks", JSON.stringify( arrayofatasks ) );
 
-}
-function getdate ()
-{
-    let data = window.localStorage.getItem( "tasks" );
-    if ( data )
-    {
-        let tasks = JSON.parse( data );
-        addElementstopagefrom( tasks );
-    }
+        getdate();
 
-}
-function delettaskw ( taskid )
-{
-    arrayofatasks = arrayofatasks.filter( ( task ) => task.id != taskid );
-    addDeta( arrayofatasks );
-}
-function torrlw ( taskid )
-{
-    for ( let i = 0; i < arrayofatasks.length; i++ )
-    {
-        if ( arrayofatasks[ i ].id == taskid )
-        {
-            arrayofatasks[ i ].completed == false ? ( arrayofatasks[ i ].completed = true ) : ( arrayofatasks[ i ].completed = false );
+        submit.onclick = function () {
+            if (input.value !== "") {
+                addTaskToArray(input.value);
+                input.value = "";
+            }
+        };
+
+        tasksdiv.addEventListener("click", (e) => {
+            if (e.target.classList.contains("p3")) {
+                deleteTask(e.target.parentElement.getAttribute("data-id"));
+                e.target.parentElement.remove();
+                updateNoItemsText(); // تحديث نص "there are no items"
+            }
+            if (e.target.classList.contains("conte")) {
+                toggleTaskStatus(e.target.getAttribute("data-id"));
+                e.target.classList.toggle("done");
+            }
+        });
+
+        function addTaskToArray(taskText) {
+            const task = {
+                id: Date.now(),
+                title: taskText,
+                completed: false,
+            };
+            arrayofatasks.push(task);
+
+            addElementToPage(task);
+            saveData();
+            updateNoItemsText(); // تحديث نص "there are no items"
         }
-    }
-    addDeta( arrayofatasks );
-}
+
+        function addElementToPage(task) {
+            let div = document.createElement("div");
+            div.className = "conte";
+            if (task.completed) {
+                div.classList.add("done");
+            }
+            div.setAttribute("data-id", task.id);
+
+            let pp = document.createElement("p");
+            pp.appendChild(document.createTextNode(task.title));
+            pp.className = "p2";
+            div.appendChild(pp);
+
+            let span = document.createElement("p");
+            span.className = "p3";
+            span.appendChild(document.createTextNode("delete"));
+            div.appendChild(span);
+
+            tasksdiv.appendChild(div);
+        }
+
+        function saveData() {
+            localStorage.setItem("tasks", JSON.stringify(arrayofatasks));
+        }
+
+        function getdate() {
+            let data = localStorage.getItem("tasks");
+            if (data) {
+                let tasks = JSON.parse(data);
+                tasks.forEach((task) => {
+                    addElementToPage(task);
+                });
+            }
+        }
+
+        function deleteTask(taskId) {
+            arrayofatasks = arrayofatasks.filter((task) => task.id != taskId);
+            saveData();
+        }
+
+        function toggleTaskStatus(taskId) {
+            arrayofatasks.forEach((task) => {
+                if (task.id == taskId) {
+                    task.completed = !task.completed;
+                }
+            });
+            saveData();
+        }
+
+        // تحديث نص "there are no items"
+        function updateNoItemsText() {
+            if (arrayofatasks.length === 0) {
+                document.querySelector(".k").style.display = "block";
+            } else {
+                document.querySelector(".k").style.display = "none";
+            }
+        }
